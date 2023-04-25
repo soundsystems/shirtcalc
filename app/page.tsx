467140048a4205b0b8b0b6dbebe295ae7c50a755
soundsystems <jaay.g@disroot.org>
2023-04-24 19:18:59 -0500

    Version 0.3
    Improved DX (renamed qty to gmt referring to garment)
    Added tooltip emoji icon
    Fixed Screen-Fee tooltip placement
    
    TODO: Fix tool-tip on mobile. Allow tip to disappear on touchEnd
    
    Coming Soon: Brand Selectors! Allow user to select between four popular blank garment manufacturers (Hanes, Gildan, Comfort Colors, Independent)

--- app/page.tsx
+++ app/page.tsx
@@ -13,21 +13,21 @@ interface Item {
 
 
 interface CalculatedItem extends Item {
-  qty: number;
+  gmt: number;
   total: number;
 }
 
 interface FormInputs {
-  qty1: number;
-  qty2: number;
-  qty3: number;
-  qty4: number;
-  qty5: number;
-  qty6: number;
-  qty7: number;
-  qty8: number;
-  qty9: number;
-  qty10: number;
+  gmt1: number;
+  gmt2: number;
+  gmt3: number;
+  gmt4: number;
+  gmt5: number;
+  gmt6: number;
+  gmt7: number;
+  gmt8: number;
+  gmt9: number;
+  gmt10: number;
   designElements: number;
   screenFee: number;
 }
@@ -60,11 +60,11 @@ const MSAShirtCalculator: React.FC = () => {
       { name: 'Long Sleeve', unitPrice: 9, darkUnitPrice: 11, designElements: 1 },
     ];
   
-    const quantities = [
-      data.qty1 || 0,
-      data.qty2 || 0,
-      data.qty3 || 0,
-      data.qty4 || 0,
+    const garments = [
+      data.gmt1 || 0,
+      data.gmt2 || 0,
+      data.gmt3 || 0,
+      data.gmt4 || 0,
     ];
   
     const screenFee = data.designElements ? data.designElements * 10 : 10;
@@ -72,9 +72,9 @@ const MSAShirtCalculator: React.FC = () => {
     const calculatedOutput: CalculatedItem[] = items.map((item, index) => {
       const { unitPrice, darkUnitPrice, ...rest } = item;
       const shirtPrice = lightShirtSelected ? unitPrice : darkUnitPrice;
-      const total = shirtPrice * quantities[index];
-      return { ...rest, qty: quantities[index], total, unitPrice: shirtPrice, darkUnitPrice };
-    }).filter(item => item.qty > 0);
+      const total = shirtPrice * garments[index];
+      return { ...rest, gmt: garments[index], total, unitPrice: shirtPrice, darkUnitPrice };
+    }).filter(item => item.gmt > 0);
   
 
     setScreenFeeSubtotal(screenFee);
@@ -103,7 +103,7 @@ const MSAShirtCalculator: React.FC = () => {
 {/* Select Dark Colored Garment */}
   <div onMouseOver={() => setTooltipOneVisible(true)} onMouseOut={() => setTooltipOneVisible(false)} onTouchStart={() => setTooltipOneVisible(!tooltipOneVisible)}>
   <input type="radio" id="dark" name="shirtColor" value="dark" checked={!lightShirtSelected} onChange={() => handleShirtSelection(false)} />
-  <label htmlFor="dark" className="relative"> Dark
+  <label htmlFor="dark" className="relative p-2">Dark<span className= "p-1">&#8505;</span>
   </label>
   <span
     className={`italic absolute z-10 px-2 py-1 text-xs bg-gray-700 text-white rounded-md mt-1 ${tooltipOneVisible ? '' : 'hidden'}`}
@@ -113,42 +113,43 @@ const MSAShirtCalculator: React.FC = () => {
   </div>
 </div>
 
-
       <hr className="mb-4" />
 <form onSubmit={handleSubmit(calcShirts)} className="space-y-4 flex flex-col items-center lg:items-end lg:flex-row lg:justify-center">
   <div className="flex flex-col items-center" onMouseOver={() => setTooltipTwoVisible(true)} onMouseOut={() => setTooltipTwoVisible(false)} onTouchStart={() => setTooltipTwoVisible(!tooltipTwoVisible)}>
-    <label htmlFor="designElements" className="lg:pr-4">Number of Design Elements:</label>
+    <label htmlFor="designElements" className="lg:pr-4"><span className= "p-1">&#8505;</span>Number of Design Elements:</label>
     <span
     className={`italic absolute z-10 px-2 py-1 text-xs bg-gray-700 text-white rounded-md mt-1 ${tooltipTwoVisible ? '' : 'hidden'}`}
   >
-    The number of graphic placements on the garment (ie. Front Left Chest + Full Back = 2)
+    The number of unique graphics on the garment <br/>
+    Note: If the same graphic is used twice, then it is only counted as one element <br/>
+    (ie. First graphic on Front Left Chest + Second Graphic on Full Back = 2 design elements)
   </span>
     <input {...register('designElements', { valueAsNumber: true, validate: validateDesignElements })} id="screenFee" placeholder="Amount" defaultValue="1" className={`border text-center border-gray-300 rounded-md p-2 ${errors.designElements && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={1}/>
     {errors.designElements && <span className="text-red-500">{errors.designElements.message}</span>}
   </div>
 
   <div className="flex flex-col items-center">
-    <label htmlFor="qty1">T-Shirt:</label>
-    <input {...register('qty1', { valueAsNumber: true, validate: validateNumber })} id="qty1" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.qty1 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/>
-    {errors.qty1 && <span className="text-red-500">{errors.qty1.message}</span>}
+    <label htmlFor="gmt1">T-Shirt:</label>
+    <input {...register('gmt1', { valueAsNumber: true, validate: validateNumber })} id="gmt1" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.gmt1 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/>
+    {errors.gmt1 && <span className="text-red-500">{errors.gmt1.message}</span>}
   </div>
 
   <div className="flex flex-col items-center">
-    <label className='pl-2' htmlFor="qty2">Crewneck:</label>
-    <input {...register('qty2', { valueAsNumber: true, validate: validateNumber })} id="qty2" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.qty2 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/>
-    {errors.qty2 && <span className="text-red-500">{errors.qty2.message}</span>}
+    <label className='pl-2' htmlFor="gmt2">Crewneck:</label>
+    <input {...register('gmt2', { valueAsNumber: true, validate: validateNumber })} id="gmt2" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.gmt2 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/>
+    {errors.gmt2 && <span className="text-red-500">{errors.gmt2.message}</span>}
   </div>
 
   <div className="flex flex-col items-center">
-    <label className='pl-2' htmlFor="qty3">Hoodie:</label>
-    <input {...register('qty3', { valueAsNumber: true, validate: validateNumber })} id="qty3" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.qty3 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/>
-    {errors.qty3 && <span className="text-red-500">{errors.qty3.message}</span>}
+    <label className='pl-2' htmlFor="gmt3">Hoodie:</label>
+    <input {...register('gmt3', { valueAsNumber: true, validate: validateNumber })} id="gmt3" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.gmt3 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/>
+    {errors.gmt3 && <span className="text-red-500">{errors.gmt3.message}</span>}
   </div>
 
   <div className="flex flex-col items-center">
-    <label className='pl-2' htmlFor="qty4">Long Sleeve:</label>
-    <input {...register('qty4', { valueAsNumber: true, validate: validateNumber })} id="qty4" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.qty4 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/> 
-    {errors.qty4 && <span className="text-red-500">{errors.qty4.message}</span>}
+    <label className='pl-2' htmlFor="gmt4">Long Sleeve:</label>
+    <input {...register('gmt4', { valueAsNumber: true, validate: validateNumber })} id="gmt4" placeholder="Quantity" className={`border text-center border-gray-300 rounded-md p-2 ${errors.gmt4 && "border-red-500"} w-1/2`} type="number" pattern="\d*" inputMode="numeric" min={0}/> 
+    {errors.gmt4 && <span className="text-red-500">{errors.gmt4.message}</span>}
 </div>
 
   <button type="submit" className="bg-blue-500 text-white ml-3 px-4 py-2 rounded-lg">Calculate Quote</button>
@@ -166,7 +167,7 @@ const MSAShirtCalculator: React.FC = () => {
     <tbody className='p-4'>
         {output.map(({ unitPrice, ...rest }, index) => (
     <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
-      <td className="border text-center px-4 py-2">{rest.qty}</td>
+      <td className="border text-center px-4 py-2">{rest.gmt}</td>
       <td className="border text-left px-4 py-2">{rest.name}</td>
       <td className="border text-center px-4 py-2">${unitPrice.toFixed(2)}</td>
       <td className="border text-center px-4 py-2">${rest.total.toFixed(2)}</td>
@@ -179,10 +180,16 @@ const MSAShirtCalculator: React.FC = () => {
   </tr>
   </tbody>
   <tfoot>
+  <span
+    className={`italic absolute z-10 px-2 py-1 text-xs bg-gray-700 text-white rounded-md mt-1 ${tooltipThreeVisible ? '' : 'hidden'}`}
+  >
+    Each design requires a ONE-TIME charge of $10 to burn the screen and keep it on hand.
+  </span>
   {output.length > 0 &&
-    <tr className="total-row" onMouseOver={() => setTooltipThreeVisible(true)} onMouseOut={() => setTooltipThreeVisible(false)} onTouchStart={() => setTooltipThreeVisible(!tooltipOneVisible)}>
-      <td className="border px-4 py-2 font-bold text-right" colSpan={3}>Screen Fee:</td>
-      <td className="border px-4 py-2 font-bold">${screenFeeSubtotal}</td>
+    <tr className="total-row">
+      <td className="border px-4 py-2 font-bold text-right" colSpan={3}><span className= "p-1">&#8505;</span>Screen Fee:
+  </td>
+      <td className="border px-4 py-2 font-bold" onMouseOver={() => setTooltipThreeVisible(true)} onMouseOut={() => setTooltipThreeVisible(false)} onTouchStart={() => setTooltipThreeVisible(!tooltipThreeVisible)}>${screenFeeSubtotal}</td>
     </tr>
   }
     <tr className="total-row">
@@ -191,11 +198,6 @@ const MSAShirtCalculator: React.FC = () => {
     </tr>
   </tfoot>
       </table>
-      <span
-    className={`italic absolute z-10 px-2 py-1 text-xs bg-gray-700 text-white rounded-md mt-1 ${tooltipThreeVisible ? '' : 'hidden'}`}
-  >
-    Each design requires a ONE-TIME charge of $10 to burn the screen and keep it on hand.
-  </span>
     </main>
     </body>
   );
